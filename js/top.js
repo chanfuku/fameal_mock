a1302dc3b61fce728384b1181db730dba27af3d8
chanfuku <k.ebato0318@gmail.com>
2017-01-23 00:27:10 +0900

    add sidebar

--- js/top.js
+++ js/top.js
@@ -1,6 +1,18 @@
-// メニュー
-$(function(){
-    $("#menubtn").click(function(){
-        $("#menu").slideToggle();
+$(document).ready(function () {
+    var sides = ["left", "top", "right", "bottom"];
+
+    // sidebar初期化
+    for (var i = 0; i < sides.length; ++i) {
+        var cSide = sides[i];
+        $(".sidebar." + cSide).sidebar({side: cSide});
+    }
+
+    // Clickハンドラー
+    $(".btn[data-action]").on("click", function () {
+        var $this = $(this);
+        var action = $this.attr("data-action");
+        var side = $this.attr("data-side");
+        $(".sidebar." + side).trigger("sidebar:" + action);
+        return false;
     });
 });
